10c76f5e3dcb3f55e1677891b0c40598339d5b22
margaretreed <margaret.r.reed@gmail.com>
2022-04-19 06:44:45 +0000

    set up flask server, and completed functions for game.py

---
+++ game.py
@@ -0,0 +1,51 @@
+import requests
+
+def generate_hidden_code():
+    """Sends GET Request to random.org API
+    and returns a list of 4 numbers as strings between 0-7"""
+
+    payload = {'num': '4',
+                'min': '0',
+                'max': '7',
+                'col': '1',
+                'base': '10',
+                'format': 'plain',
+                'rnd': 'new'}
+
+    req = requests.get('https://www.random.org/integers/', params=payload)
+    
+    hidden_code = []
+    for num in req.text.splitlines():
+        hidden_code.append(num)
+
+    return hidden_code
+
+def is_guess_correct(hidden_code, player_guess):
+    """Takes in two lists and returns a bool to indicate if the two lists are identical"""
+    if hidden_code == player_guess:
+        return True
+    
+    return False
+
+def get_num_correct(hidden_code, player_guess):
+    """Takes in two lists and returns the number of commonalities in that list"""
+    num_correct = 0
+    for num in player_guess:
+        if num in hidden_code:
+            num_correct+=1
+            hidden_code.remove(num)
+    
+    return num_correct
+
+def get_num_correct_loc(hidden_code, player_guess):
+    """Takes in two lists and returns """
+    num_correct_loc = 0
+    for idx, num in enumerate(player_guess):
+        if num == hidden_code[idx]:
+            num_correct_loc+=1
+    
+    return num_correct_loc
+
+# hidden_code = ['4', '3', '2', '1']
+# player_guess = ['3', '4', '0', '4']
+# print(get_num_correct_loc(hidden_code, player_guess))
---
+++ server.py
@@ -0,0 +1,17 @@
+"""Server for Mastermind Game"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/mastermind")
+def mastermind():
+    return render_template("gameboard.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
